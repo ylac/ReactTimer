a58b6797532087c18fd4ca7b3f58d7bfbc3d6171
Caly <caly@inventa.com.au>
2017-02-22 16:15:03 +1100

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 
 //Load foundation
@@ -14,6 +16,8 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <IndexRoute component={Timer}/>
+      <Route path="countdown" component={Countdown}/>
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -10,10 +10,10 @@ var Nav = () => {
             React Timer App
           </li>
           <li>
-            <IndexLink to="/" activeClassname="active-link">Timer</IndexLink>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
           </li>
           <li>
-            <Link to="/" activeClassname="active-link">Countdown</Link>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
           </li>
         </ul>
       </div>
